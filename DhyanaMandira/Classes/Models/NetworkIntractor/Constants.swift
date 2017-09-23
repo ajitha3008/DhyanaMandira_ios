c81c58c955bb8974fdc9b487a670b09a2782674b
Abilash <abilashblnair@gmail.com>
2017-09-23 16:03:33 +0530

    Gallery screen issue and model class added

---
+++ DhyanaMandira/Classes/Models/NetworkIntractor/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  DhyanaMandira
+//
+//  Created by Abilash Cumulations on 14/08/17.
+//  Copyright © 2017 AjithaYasmin. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+
+    struct GoogleSheetURL {
+        static let EventURL = "https://spreadsheets.google.com/tq?key=1FBJMP9b-0I1hJiGFPhvcgB3ZOnUQNElL6hI9vtQg0EM"
+        static let CommentsURL = "https://spreadsheets.google.com/tq?key=1JyIZM8QKG9rBGQBWNydv7472C12ZwoamT2UR6ymiikk"
+    }
+    
+}
